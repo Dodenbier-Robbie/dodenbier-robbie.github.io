43d2e1233e591785a2e37906348c85d99c3bb804
Robbie Dodenbier <rdodenbier@byui.edu>
2017-10-02 14:23:38 -0700

    HTML 5

---
+++ cit261/scripts/localStorage.js
@@ -0,0 +1,18 @@
+function clickCounter() {
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount) + 1;
+        } else {
+            localStorage.clickcount = 1;
+        }
+        
+        document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
+    } else {
+        document.getElementById("result").innerHTML = "Sorry, time to upgrade your browser";
+    }
+}
+
+function clearItems () {
+    window.localStorage.clear();
+    location.reload();
+}
