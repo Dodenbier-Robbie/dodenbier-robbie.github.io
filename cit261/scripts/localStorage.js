1dbc318b20249f9c9c4f9705651c7a94de109935
Robbie Dodenbier <rdodenbier@byui.edu>
2017-10-02 17:32:07 -0700

    HTML 5

--- cit261/scripts/localStorage.js
+++ cit261/scripts/localStorage.js
@@ -16,3 +16,21 @@ function clearItems () {
     window.localStorage.clear();
     location.reload();
 }
+
+function populatePerson () {
+    var userFirstName = document.getElementById("firstName").value;
+    var userLastName = document.getElementById("lastName").value;
+    var userEmail = document.getElementById("email").value;
+    var userPhone = document.getElementById("phone").value;
+    
+    var person = {firstName: userFirstName, lastName: userLastName, email: userEmail, phone: userPhone};
+    
+    localStorage.setItem('user', JSON.stringify(person));
+    
+    document.getElementById("userOutput").innerHTML = "Your submission is complete";  
+}
+
+function setUser() {
+    var user = JSON.parse(localStorage.getItem('user'));
+    document.getElementById("userOutput").innerHTML = "<b>First Name:</b> " + user.firstName + "<br><b>Last Name:</b> " + user.lastName + "<br><b>Email:</b> " + user.email + "<br><b>Phone: " + user.phone;
+}
