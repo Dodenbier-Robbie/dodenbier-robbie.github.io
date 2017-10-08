89d2bb94e2b7c16c4758a718b48f5dc7ccc6d4b5
Robbie Dodenbier <rdodenbier@byui.edu>
2017-10-07 20:32:29 -0600

    HTML 03

--- cit261/scripts/jsonFeed.js
+++ cit261/scripts/jsonFeed.js
@@ -4,22 +4,12 @@ function testJSON() {
     xhr.onreadystatechange = function() {
       if (xhr.readyState == 4) {
           var response = JSON.parse(xhr.responseText);
-          var jsonID = ""; 
-          var jsonFirstName = "";
-          var jsonLastName = "";
-          var jsonEmail = "";
-          var jsonGender = "";
-          var jsonIPAddress = "";
+          var jsonDisplay = "";
           
           for(var i = 0; i < response.length; i++) {
-              jsonID += response[i].id + "<br>";
-              jsonFirstName += response[i].first_name + "<br>";
-              jsonLastName += response[i].last_name + "<br>";
-              jsonEmail += response[i].email + "<br>";
-              jsonGender += response[i].gender + "<br>";
-              jsonIPAddress += response[i].ip_address + "<br>";
+              jsonDisplay += "<p>" + response[i].id + "<br>" + response[i].first_name + "<br>" + response[i].last_name + "<br>" + response[i].email + "<br>" + response[i].gender + "<br>" + response[i].ip_address + "</p>";
             }
-          document.getElementById("jsonOutput").innerHTML = jsonID;
+          document.getElementById("jsonOutput").innerHTML = jsonDisplay;
       }
     }
 xhr.send();   
