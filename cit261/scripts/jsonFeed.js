656d3edc6108d19e1c22fb482aacfa88d29eed78
Robbie Dodenbier <rdodenbier@byui.edu>
2017-10-07 21:04:03 -0600

    HTML 03

--- cit261/scripts/jsonFeed.js
+++ cit261/scripts/jsonFeed.js
@@ -11,7 +11,7 @@ function testJSON() {
             }
           document.getElementById("jsonOutput").innerHTML = jsonDisplay;
           
-          var jsonString = JSON.stringify(xhr.responseText,null,4);
+          var jsonString = JSON.stringify(xhr.responseText);
           
           document.getElementById("jsonStringOutput").innerHTML = "<p><b>JSON converted to a string</b></p>" + jsonString;
       }
