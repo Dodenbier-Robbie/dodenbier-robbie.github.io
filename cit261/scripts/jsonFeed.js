d734d2a70f80a28c57677f623656b218f8a6811c
Robbie Dodenbier <rdodenbier@byui.edu>
2017-10-07 20:27:21 -0600

    HTML 03

--- cit261/scripts/jsonFeed.js
+++ cit261/scripts/jsonFeed.js
@@ -4,9 +4,20 @@ function testJSON() {
     xhr.onreadystatechange = function() {
       if (xhr.readyState == 4) {
           var response = JSON.parse(xhr.responseText);
-          var jsonID = "";  
+          var jsonID = ""; 
+          var jsonFirstName = "";
+          var jsonLastName = "";
+          var jsonEmail = "";
+          var jsonGender = "";
+          var jsonIPAddress = "";
+          
           for(var i = 0; i < response.length; i++) {
-              jsonID += response[i].id + "<br>";   
+              jsonID += response[i].id + "<br>";
+              jsonFirstName += response[i].first_name + "<br>";
+              jsonLastName += response[i].last_name + "<br>";
+              jsonEmail += response[i].email + "<br>";
+              jsonGender += response[i].gender + "<br>";
+              jsonIPAddress += response[i].ip_address + "<br>";
             }
           document.getElementById("jsonOutput").innerHTML = jsonID;
       }
