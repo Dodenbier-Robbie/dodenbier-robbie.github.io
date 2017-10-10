8664a198b3eb20d2ac0c080367db3641489a8d6a
Robbie Dodenbier <rdodenbier@byui.edu>
2017-10-09 21:03:18 -0600

    HTML 07

---
+++ cit261/scripts/cssManipulation.js
@@ -0,0 +1,8 @@
+function changePictureWidth() {
+    
+    document.getElementById("myPic").style.width = "40%"; 
+}
+
+function changeFontSize() {
+    document.getElementById("fontChange").style.fontSize = "20px";
+}
