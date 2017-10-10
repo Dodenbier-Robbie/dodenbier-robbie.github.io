3c6aae24d9ba011830efe4a29f9e03cc7699b87b
Robbie Dodenbier <rdodenbier@byui.edu>
2017-10-10 16:02:50 -0600

    HTML 03

--- cit261/scripts/jsonFeed.js
+++ cit261/scripts/jsonFeed.js
@@ -18,3 +18,31 @@ function testJSON() {
     }
 xhr.send();   
 }
+
+function ipJSON() {
+ var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://ip.jsontest.com/", true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+          var response = JSON.parse(xhr.responseText);
+          var jsonString = JSON.stringify(response, null, 4);
+          
+          document.getElementById("jsonStringOutput").innerHTML = "<hr \><p><b>JSON converted to a string</b></p><pre>" + jsonString + "</pre>";
+      }
+    }
+xhr.send();   
+}
+
+function dateTimeJSON() {
+ var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://date.jsontest.com", true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+          var response = JSON.parse(xhr.responseText);
+          var jsonString = JSON.stringify(response, null, 4);
+          
+          document.getElementById("jsonStringOutput").innerHTML = "<hr \><p><b>JSON converted to a string</b></p><pre>" + jsonString + "</pre>";
+      }
+    }
+xhr.send();   
+}
