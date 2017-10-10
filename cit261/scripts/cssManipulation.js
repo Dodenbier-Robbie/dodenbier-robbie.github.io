1c005c5593bb87a1d9d55216c585fbbae1678735
Robbie Dodenbier <rdodenbier@byui.edu>
2017-10-10 08:25:28 -0600

    HTLM 07

--- cit261/scripts/cssManipulation.js
+++ cit261/scripts/cssManipulation.js
@@ -6,3 +6,13 @@ function changePictureWidth() {
 function changeFontSize() {
     document.getElementById("fontChange").style.fontSize = "20px";
 }
+
+function changeFontColor() {
+    var colorValue = document.getElementById("fontColor").value;
+    document.getElementById("fontChange").style.color = colorValue;
+}
+
+function clearItems() {
+    window.localStorage.clear();
+    location.reload();
+}
