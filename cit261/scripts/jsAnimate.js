e8f8abad86037209400ebcb9e56b387bc7b074a1
Robbie Dodenbier <rdodenbier@byui.edu>
2017-10-23 18:58:02 -0600

    HTML 08

--- cit261/scripts/jsAnimate.js
+++ cit261/scripts/jsAnimate.js
@@ -1,4 +1,4 @@
-function myAnimate() {
+function sunSet() {
     var sun = document.getElementById("animate");
     var sky = document.getElementById("container");
     var pos = 5;
@@ -15,6 +15,27 @@ function myAnimate() {
         } else {
         pos++; 
         sun.style.top = pos + 'px';
+        }
+    }
+}
+
+function sunRise() {
+    var sun = document.getElementById("animate");
+    var sky = document.getElementById("container");
+    var pos = 300;
+    var id = setInterval(frame, 10);    
+    sky.style.transition = "opacity 3s linear 0s";
+    sky.style.opacity = 1;
+    
+    function frame() {
+        if (pos == 5) {
+        clearInterval(id);
+        } else {
+        pos--; 
+        sun.style.top = pos + 'px';
+        sun.style.borderRadius = "50px";
+        sun.style.transition = "opacity 0s linear 0s";
+        sun.style.opacity = 1;
         }
     }
 }
