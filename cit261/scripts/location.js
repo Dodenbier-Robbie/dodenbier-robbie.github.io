d0dc0e365e3d2cdc14f097a3c0626958800f3b97
Robbie Dodenbier <rdodenbier@byui.edu>
2017-10-24 16:30:56 -0600

    Project HTML

--- cit261/scripts/location.js
+++ cit261/scripts/location.js
@@ -1,28 +1,63 @@
 function geoFindMe() {
-  var output = document.getElementById("out");
+    var output = document.getElementById("out");
+    
+    navigator.geolocation.getCurrentPosition(success, error);
 
-  if (!navigator.geolocation){
-    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    if (!navigator.geolocation){
+        output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+        return;
+    }
+
+    function success(position) {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+            weatherGeoJSON(latitude, longitude);
+        }
+
+    function error() {
+        output.innerHTML = "Unable to retrieve your location";
+    }
+    
     return;
-  }
+}
 
-  function success(position) {
-    var latitude  = position.coords.latitude;
-    var longitude = position.coords.longitude;
+function weatherGeoJSON(lat, long) {
+    var url = "http://api.wunderground.com/api/971a6113214d3607/geolookup/q/" + lat + "," + long + ".json"
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url, true);
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          var response = JSON.parse(this.responseText);
+          var city = response.location.city;
+          var country = response.location.country;
+          var state = response.location.state;
+          weatherJSON(city, state, country);
+      }
+    }
+xhttp.send();   
+}
 
-    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
-
-    var img = new Image();
-    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
-
-    output.appendChild(img);
-  }
-
-  function error() {
-    output.innerHTML = "Unable to retrieve your location";
-  }
-
-  output.innerHTML = "<p>Locating…</p>";
-
-  navigator.geolocation.getCurrentPosition(success, error);
+function weatherJSON(city, state, country) {
+    var url = "http://api.wunderground.com/api/971a6113214d3607/forecast/q/" + country + "/" + state + "/" + city + ".json"
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url, true);
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          var response = JSON.parse(this.responseText);
+          
+          console.log(response.forecast.txt_forecast.forecastday.length);
+          
+          var day = [];
+          var text = [];
+          for(var i = 0; i < response.forecast.txt_forecast.forecastday.length; i++) {
+              day.push(response.forecast.txt_forecast.forecastday[i].title);
+              text.push(response.forecast.txt_forecast.forecastday[i].fcttext);
+          }
+          
+          for (var i = 0; i < day.length; i++) {
+              document.getElementById("day").innerHTML = day[i];
+          }
+      }
+    }
+xhttp.send();   
 }
